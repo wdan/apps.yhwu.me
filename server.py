222b05b6be52924518e20fe07fcc01155891cdf7
Yanhong Wu <wdan1990@gmail.com>
2014-04-27 09:22:14 -0400

    Init

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "hello world!"
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == "main":
+    app.run()
